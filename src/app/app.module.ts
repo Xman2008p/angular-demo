827fd7e74ab7b7464270e292e490d74897224acf
xman2008p <xman2008p@gmail.com>
2023-04-14 16:35:54 +0800

    加入英雄列表：火焰鳥、及四大技能

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    Page2Component
+    Page2Component,
+    HeroFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,2 +1,9 @@
 export class Hero {
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+    // 其中 alterEgo 屬性後面接了 ? 號，代表 alterEgo 屬性不是必須的，呼叫建構函式時這個參數可以省略。
+  ) {}
 }
